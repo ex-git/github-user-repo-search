9288746e03befc01d84c77245fc3d151c8b52d76
Evan Xu <27160136+ex-git@users.noreply.github.com>
2018-10-04 12:02:34 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const searchURL = 'https://api.github.com';
+
+function fetchAPI(name) {
+    const options = {
+        headers: new Headers({
+            "Accept": "application/vnd.github.mercy-preview+json"})
+        };
+    fetch(searchURL + '/users/' + name + '/repos', options)
+    .then(response => response.json())
+    .then(responseJson => renderHTML(responseJson)).catch(error => alert("error"))
+}
+
+function renderHTML(source) {
+    $('.js-result').empty();
+    for(let i=0; i<source.length; i++) {
+        let {description, html_url} = source[i];
+        $('.js-result').append(`<p>${description}<br><a href="${html_url}" target="_blank">${html_url}</a></p>`)
+    }
+    $('.js-result').prop('hidden', false)
+}
+
+function catchSubmit() {
+    $('.searchById').on('submit', event => {
+        event.preventDefault();
+        let keyword= $('input[name="userID"]').val();
+        fetchAPI(keyword);
+    })
+}
+
+$(catchSubmit)
